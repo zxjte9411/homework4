c7dba28312b57aa48b22cfe6ce5f3dbcff16b44f
zxjte9411 <a5852241@gmail.com>
2019-12-24 09:36:57 +0800

    fix the logic

--- src/com/zxjte9411/LetterGrade.java
+++ src/com/zxjte9411/LetterGrade.java
@@ -5,13 +5,13 @@ public class LetterGrade {
         char grade;
         if (score <0 || score > 100)
             grade = 'X';
-        else if (score>=90 && score <=100)
+        else if (score >= 90)
             grade = 'A';
-        else if (score>=80 && score <90)
+        else if (score >= 80)
             grade = 'B';
-        else if (score>=70 && score <80)
+        else if (score >= 70)
             grade = 'C';
-        else if (score>=60 && score <70)
+        else if (score >= 60)
             grade = 'D';
         else
             grade ='F';
